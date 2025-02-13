99891da5af8c00898117f620c5cc0fee3be68f99
WINNIE <user@192.168.1.10>
2025-02-13 14:39:08 +0100

    Initial commit

--- models/gold_holding.go
+++ models/gold_holding.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type GoldHolding struct {
 	gorm.Model
-	UserID      	uint 	`gorm:"not null"`
-	Weight			uint 	`gorm:"not null"`
+	UserID      	uint 		`gorm:"not null"`
+	Weight				uint 		`gorm:"not null"`
 	CurrentValue 	float64 `gorm:"not null"`
-	Status  		string 	`gorm:"not null"`
+	Status  			string 	`gorm:"not null"`
 }
