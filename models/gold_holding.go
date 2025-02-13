2a352feb88b04348adce44067cf852b30f4560ab
Sam Iso <samdoxiso@gmail.com>
2025-02-13 15:18:44 +0100

    Initial commit

--- models/gold_holding.go
+++ models/gold_holding.go
@@ -9,5 +9,4 @@ type GoldHolding struct {
 	CurrentValue 	float64 	`gorm:"not null"`
 	GoldType 		string	 	`gorm:"not null"`
 	Status  		string 		`gorm:"type:enum('active','pledged','liquidated'); default:'active'"`
-=======
 }
