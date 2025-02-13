14df210045afc2a437320ce5475ab8d57043b954
winifredaj <140772756+winifredaj@users.noreply.github.com>
2025-02-13 15:31:24 +0100

    Update margin_call.go

--- models/margin_call.go
+++ models/margin_call.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 type MarginCall struct{
 	gorm.Model        
 
-	ID  								uint			`gorm:"primaryKey"`
-	LeverageID 					uint			`gorm:"not null"`
-	RequiredCollateral  float64		`gorm:"not null"`
-	Status  						string 		`gorm:"type:enum('pending','resolved','defaulted'); default:'pending'"`	
+	ID  			uint		`gorm:"primaryKey"`
+	LeverageID 	 	uint		`gorm:"not null"`
+	RequiredCollateral  	float64		`gorm:"not null"`
+	Status  		string 		`gorm:"type:enum('pending','resolved','defaulted'); default:'pending'"`	
 }
