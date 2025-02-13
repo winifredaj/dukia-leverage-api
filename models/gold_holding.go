14f814b54178c3930a88c0a43f47a3bd20d7bc39
winifredaj <140772756+winifredaj@users.noreply.github.com>
2025-02-13 15:30:35 +0100

    Update gold_holding.go

--- models/gold_holding.go
+++ models/gold_holding.go
@@ -4,10 +4,10 @@ import "gorm.io/gorm"
 
 type GoldHolding struct {
 	gorm.Model
-	UserID      	uint 			`gorm:"not null"`
-	Weight				float64 	`gorm:"not null"`
+	UserID      	uint 		`gorm:"not null"`
+	Weight		float64 	`gorm:"not null"`
 	CurrentValue 	float64 	`gorm:"not null"`
-	GoldType 			string	 	`gorm:"not null"`
-	Status  			string 		`gorm:"type:enum('active','pledged','liquidated'); default:'active'"`
+	GoldType 	string	 	`gorm:"not null"`
+	Status  	string 		`gorm:"type:enum('active','pledged','liquidated'); default:'active'"`
 
 }
